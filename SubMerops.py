da7303536cc90c8f3d12965b044050e716b8366a
Robert Qiao <RobertQiao@Roberts-MacBook-Pro-2.local>
2017-02-11 22:00:29 +1030

    Unique Entry Filter Added

--- SubMerops.py
+++ SubMerops.py
@@ -39,9 +39,14 @@ if __name__ == "__main__":
 	wfile = open (writefilefullpath,"w")
     		
 	wfile.write("source\ttarget\n")
+	peptidases = []
 	for dataset in datasets:
 		for index, item in dataset:
 			if ((index == "Peptidase") & (item.lower() != "unknown peptidase")) :
-				wfile.write (SubID+"\t"+str(item)+"\n")
+				peptidases.append(item)
+	peptidases = set(peptidases)
+	print peptidases
+	for peptidase in peptidases:
+		wfile.write (SubID+"\t"+str(peptidase)+"\n")
 
 	wfile.close()
